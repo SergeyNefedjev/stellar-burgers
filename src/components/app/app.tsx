1c9dd95c7231c0c072e83f2340c9aedb7592c6b3
Sergey Nefedjev <sergeynefedjev@mail.ru>
2025-06-05 15:38:23 +0900

    пробный коммит

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,22 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 const App = () => (
+  <Router>
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+  <AppHeader />
+  <Routes>
+    <Route path = '/' element = {<ConstructorPage/>} />
+    <Route path="/feed" element={<Feed />} />
+  </Routes>
   </div>
+</Router>  
 );
 
 export default App;
